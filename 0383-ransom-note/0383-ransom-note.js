8ff0f1401a7eb46e5d8c387c22034ddc0c422e2b
vjvl95 <62174495+vjvl95@users.noreply.github.com>
2023-02-09 20:11:49 +0900

    Time: 98 ms (57.67%), Space: 46.7 MB (26.01%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+    let obj={}
+    
+    ransomNote.split("").forEach((v)=>{
+        obj[v]?obj[v]+=1:obj[v]=1
+    })
+    
+    magazine.split("").forEach((v)=>{
+        obj[v]?obj[v]-=1:obj[v]=0
+    })
+    console.log(obj)
+    for(item of Object.values(obj)){
+        if(item!==0) return false
+    }
+    return true
+};
